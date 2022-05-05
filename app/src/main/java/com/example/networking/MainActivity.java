761f87bb7d518f4b75282ff1c4dd087522576e09
e21krida <e21krida@student.his.se>
2022-05-05 20:11:57 +0200

    tillfogade GSON parser i MainActivity.java samt gav applikationen tillgångt till internet inom AndroidManifest.xml

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -39,5 +40,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Mountains>>() {}.getType();
+        ArrayList<Mountains> listOfMountains = gson.fromJson(json, type);
     }
 }
