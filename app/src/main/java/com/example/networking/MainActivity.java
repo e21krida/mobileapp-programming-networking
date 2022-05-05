e4837e0d8e26a1bb8a074ee88ba320ee96b7f47e
e21krida <e21krida@student.his.se>
2022-05-05 16:41:29 +0200

    skapade min ArrayList av <Mountains> med tillhörande Mountains.java, innehållandes getters and setters

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,29 +4,37 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+    private final String TAG = "==>";
 
     private RecyclerView recyclerView;
+    private ArrayList<Mountains> listOfMountains;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         new JsonTask(this).execute(JSON_URL);
 
         recyclerView = findViewById(R.id.recyler_view);
+        listOfMountains = new ArrayList<Mountains>();
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
     }
-
 }
---
+++ app/src/main/java/com/example/networking/Mountains.java
@@ -0,0 +1,29 @@
+package com.example.networking;
+
+import androidx.annotation.NonNull;
+
+public class Mountains {
+    private String name;
+    private int size;
+
+    public Mountains(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
